d86306c26d3763b34e46716334fecd46a318df59
Irinaabieva <irinaabieva03@gmail.com>
2022-12-07 11:39:56 +0300

    Задание 3

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,52 @@
 public class Car {
+    private final String brand;
+    private final String model;
+    private final double engineVolume;
+    private final String color;
+    private final int year;
+    private final String country;
 
+    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    @Override
+    public String toString() {
+        return "Автомобиль: " +
+                "Марки = " + brand +
+                ", модель " + model +
+                ", объем двигателя " + engineVolume +
+                ", цвет " + color +
+                ", год " + year +
+                ", страна " + country ;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        Car lada = new Car("Lada", "Granta", 1.7, "Желтый", 2015, "Россия");
+        Car audi = new Car("Audi", "A8", 3.0, "черный", 2020, "Германия");
+        Car bmw = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия");
+        Car kia = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея");
+        Car hyundai = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
+        printCar(lada);
+    }
+    public static void printCar(Car car) {
+        System.out.println(car.toString());
     }
 }
